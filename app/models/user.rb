7d6e8f23d37dac05006cdbe5409c33273df310a1
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-01-14 10:21:36 -0700

    [feature] add user migration/model and validations

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :username, presence: true,
+            uniqueness: { case_scensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true,
+            length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
+end
