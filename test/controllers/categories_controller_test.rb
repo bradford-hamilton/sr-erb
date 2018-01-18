3614054963c15248e06d37b074a5fb0e2a85d580
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-01-17 20:19:35 -0700

    [feature] add article <-> categories assossiation, tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: 'sports')
+    @user = User.create(username: 'Josce', email: 'Josce@big_idiot.com', password: 'password', admin: true)
   end
 
   test 'should get index' do
@@ -11,6 +12,8 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new' do
+    byebug
+    sign_in_as(@user, 'password')
     get new_category_path
     assert_response :success
   end
@@ -18,5 +21,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test 'should get show' do
     get category_path(@category)
     assert_response :success
+  end
+
+  test 'should redirect create when admin not logged in' do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: 'sports' } }
+    end
+    assert_redirected_to categories_path
   end
 end
